9ce6de10a269f388360cada0f643f8e069de77b7
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-13 10:28:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin() {
+    const record = {
+        year: "2015",
+        result: "w",
+
+    };
+}
+superbowlWin = (record)=>{
+    const result =record.find(record=>record.result==="w");
+    return !!result? result.year:undefined;
+
+}
